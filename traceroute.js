68f1c4064fca04e45c4dd98cbbd0420533501aaa
jorgev259 <jorgev259@outlook.es>
2019-02-19 16:55:31 -0600

    Update traceroute.js

--- traceroute.js
+++ traceroute.js
@@ -23,26 +23,18 @@ class Traceroute extends Process {
     parseHop(hopData) {
         const regex = /^\s*(\d+)\s+(?:([a-zA-Z0-9:.]+)\s+([0-9.]+\s+ms)|(\*))/;
         console.log(hopData)
-        const parsedData = new RegExp(regex, '').exec(hopData);
+        // const parsedData = new RegExp(regex, '').exec(hopData);
 
         let result = null;
+        let parsedData = hopData.split(' ')
+        if(parsedData[0] !=== "traceroute"){
+            result.hop = parseInt(parsedData[0], 10)
+            if(parsedData[2]) result.ip = `${parsedData[1]} ${parsedData[2]}`
+            else result.ip = parsedData[1]
+            if(parsedData[3]) result.rtt1 = `${parsedData[3]} ${parsedData[4]}`
+        }    
         console.log(parsedData)
-        if (parsedData !== null) {
-            if (parsedData[4] === undefined) {
-                result = {
-                    hop: parseInt(parsedData[1], 10),
-                    ip: parsedData[2],
-                    rtt1: parsedData[3]
-                };
-            }
-            else {
-                result = {
-                    hop: parseInt(parsedData[1], 10),
-                    ip: parsedData[4],
-                    rtt1: parsedData[4]
-                };
-            }
-        }
+        console.log(result)
 
         return result;
     }
